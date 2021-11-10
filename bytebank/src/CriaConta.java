242b95a9ba43a7cf37cd5e0097a78176eee96310
Leoni Cesar <leonecezar99@gmail.com>
2021-11-10 00:27:19 -0300

    Projeto Inicial

---
+++ bytebank/src/CriaConta.java
@@ -0,0 +1,20 @@
+public class CriaConta {
+
+    public static void main(String[] args) {
+        Conta primeiraConta = new Conta(2424, 2626);
+        primeiraConta.deposita(200);
+        System.out.println(primeiraConta.getSaldo());
+
+        primeiraConta.deposita(100);
+        System.out.println(primeiraConta.getSaldo());
+
+        Conta segundaConta = new Conta(36363,83838);
+        segundaConta.deposita(50);
+
+        System.out.println("primeira conta tem " + primeiraConta.getSaldo());
+        System.out.println("segunda conta tem " + segundaConta.getSaldo());
+
+        System.out.println("referenciar: " + primeiraConta);
+    }
+
+}
---
+++ bytebank/src/testaValores.java
@@ -0,0 +1,13 @@
+public class testaValores {
+
+    public static void main( String [] args) {
+ // A CONTA ESTÁ INVALIDA
+   Conta conta = new Conta(1344,2536);
+
+   Conta conta2 = new Conta (1344,2536);
+   System.out.println(conta.getTotal());
+
+
+    }
+
+}
